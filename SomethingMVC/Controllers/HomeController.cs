9a205b0c101703eabc6d4c2453467f1f7dc2857a
CSMRCK <incept328@gmail.com>
2021-01-25 17:22:42 +0200

    main commit

---
+++ SomethingMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SomethingMVC.Models;
+using System.Linq;
+
+namespace SomethingMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        MobileContext db;
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.PhoneId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return "Thank you," + order.User + ", for order!";
+        }
+    }
+}
---
+++ SomethingMVC/Models/MobileContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SomethingMVC.Models
+{
+    public class MobileContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public MobileContext(DbContextOptions<MobileContext> options): base(options)
+        {
+            // вызов Database.EnsureCreated() при отсутствии базы данных автоматически создает ее.
+            Database.EnsureCreated();
+        }
+    }
+}
